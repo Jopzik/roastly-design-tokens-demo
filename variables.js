63cb7d620d65c5a5d56987bcf297431c33f45667
jopzik <jmora@ia.com.mx>
2024-11-06 13:44:58 -0600

    Create Tokens

---
+++ variables.js
@@ -0,0 +1,16 @@
+/**
+ * Do not edit directly, this file was auto-generated.
+ */
+
+export const textDefault = "#3d3d3d";
+export const textAccent = "#000000";
+export const textBrand = "#000000";
+export const textOnColor = "#f5f5f5";
+export const textLink = "#000000";
+export const backgroundBrand = "#2b2b2b";
+export const backgroundBody = "#ffffff";
+export const backgroundElement = "#ffffff";
+export const borderDefault = "#d9d9d9";
+export const borderInput = "#d9d9d9";
+export const borderButton = "#d9d9d9";
+export const radius = "8px";
