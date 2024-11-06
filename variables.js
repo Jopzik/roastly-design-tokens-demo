502f8f63ead988f96af22ff64bbf5ce3bacd8caa
jopzik <jmora@ia.com.mx>
2024-11-06 14:37:12 -0600

    Commit

--- variables.js
+++ variables.js
@@ -2,15 +2,15 @@
  * Do not edit directly, this file was auto-generated.
  */
 
-export const textDefault = "#484848";
+export const textDefault = "#3d3d3d";
 export const textAccent = "#000000";
-export const textBrand = "#da0663";
-export const textOnColor = "#ffffff";
-export const textLink = "#da0663";
-export const backgroundBrand = "#da0663";
-export const backgroundBody = "#f2f2f2";
+export const textBrand = "#000000";
+export const textOnColor = "#f5f5f5";
+export const textLink = "#000000";
+export const backgroundBrand = "#2b2b2b";
+export const backgroundBody = "#ffffff";
 export const backgroundElement = "#ffffff";
-export const borderDefault = "#bfbebe";
-export const borderInput = "#d1d1d1";
-export const borderButton = "#d1d1d1";
-export const radius = "0px";
+export const borderDefault = "#d9d9d9";
+export const borderInput = "#d9d9d9";
+export const borderButton = "#d9d9d9";
+export const radius = "8px";
