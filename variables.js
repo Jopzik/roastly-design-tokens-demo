70989817143c358adb18c7f9fd652477de9d9ddc
jopzik <jmora@ia.com.mx>
2025-01-08 19:51:35 -0700

    New tokens

--- variables.js
+++ variables.js
@@ -2,15 +2,26 @@
  * Do not edit directly, this file was auto-generated.
  */
 
-export const textDefault = "#cbcdd0";
-export const textAccent = "#fefefe";
-export const textBrand = "#6eb3ee";
-export const textOnColor = "#ffffff";
-export const textLink = "#6eb3ee";
-export const backgroundBrand = "#0c8ce9";
-export const backgroundBody = "#282a37";
-export const backgroundElement = "#323644";
-export const borderDefault = "#484848";
-export const borderInput = "#313644";
-export const borderButton = "#484848";
-export const radius = "8px";
+export const colorBackgroundBrandDefault = "#0c8ce9";
+export const colorBackgroundBrandHover = "#0a7acb";
+export const colorBackgroundLayerElement = "#323644";
+export const colorBackgroundLayerBody = "#282a38";
+export const colorBackgroundLayerSurface01Default = "#000000";
+export const colorTextContentOnColor = "#ffffff";
+export const colorTextContentDefault = "#cbcdd0";
+export const colorTextContentProminent = "#fefefe";
+export const colorTextContentLight = "#78818f";
+export const colorTextContentLighter = "#747474";
+export const colorTextContentBrand = "#6fb3ee";
+export const colorTextContentLink = "#6fb3ee";
+export const colorBorderDefault = "#484848";
+export const colorIconContentProminent = "#fefefe";
+export const colorIconContentBrand = "#6fb3ee";
+export const colorIconContentDefault = "#cbcdd0";
+export const colorIconContentLight = "#78818f";
+export const colorIconContentLighter = "#747474";
+export const colorIconContentOnColor = "#ffffff";
+export const componentButtonRadius = "0.5rem";
+export const componentButtonBorder = "#484848";
+export const componentInputRadius = "0.5rem";
+export const componentInputBorder = "#323644";
